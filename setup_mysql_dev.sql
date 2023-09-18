669cdd1dcde13d88f5e5a6176581a9a3b5ec4651
XimeonLeo <ximeonleo@gmail.com>
2023-09-18 11:54:01 -0700

    Fixing error in myswl syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,12 @@
 -- A script that prepares MySQL database
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 
--- create a user IN localhost
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhos' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Granting all permission to user on this database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+-- CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+-- GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
