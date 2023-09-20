811b8a3f84e124f09851e1371c206e94c910abc6
XimeonLeo <ximeonleo@gmail.com>
2023-09-20 14:34:35 -0700

    Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,7 +40,7 @@ class DBStorage:
         else:
             result = self.__session.query(State).all()
             result.extend(self.__session.query(City).all())
-            # result.extend(self.__session.query(User).all())
+            result.extend(self.__session.query(User).all())
             # result.extend(self.__session.query(Amenity).all())
             # result.extend(self.__session.query(Place).all())
             # result.extend(self.__session.query(Review).all())
