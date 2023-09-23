032d0e69ca99be1c577be8d3418ed302b9c9b637
XimeonLeo <ximeonleo@gmail.com>
2023-09-23 03:36:57 +0100

    moving to other computer

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import os
 storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
+    """Defines a class Amenity """
+    __tablename__ = amenities
     if storage_type == "db":
-        pass
+        name = Column(String(128),
+                      nullable=False)
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -3,8 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Table
 import os
 storage_type = os.environ.get("HBNB_TYPE_STORAGE")
+
+if storage_type == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False)
+                        )
 
 
 class Place(BaseModel, Base):
@@ -42,10 +53,26 @@ class Place(BaseModel, Base):
         reviews = relationship("Review",
                                backref="place",
                                cascade="all, delete")
+        amenities = relationship('Amenity',
+                                 secondary=place_amenity,
+                                 viewonly=False,
+                                 backref='place_amenities')
     else:
+        city_id = ""
+        user_id = "" 
+        name = "" 
+        description = ""                                                   number_rooms = 0
+        number_bathrooms = 0 
+        max_guest = 0     
+        price_by_night = 0 
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
-            """Returns A list of review associated with this state
+            """Returns A list of review associated with
+                this place
             """
             from models import storage, Review
             reviews = []
@@ -53,3 +80,25 @@ class Place(BaseModel, Base):
                 if review.places_id == self.id:
                     reviews.append(review)
             return review
+
+        @property
+        def amenities(self):
+            """Returns A list of amenities associated with
+                this place
+            """
+            from models import Amenity
+            amenities[]
+            for amenity in storage.all(Amenity).values():
+                if amenity._id == self.amenity_ids:
+                    amenities.append(amenity)
+            return amenities
+
+        @amenities.setter
+        def amenities(self, amenity_obj):
+            """Handles append method for adding Amenity.id
+                to the attribute amenity_ids
+            """
+            if amenity_obj:
+                if isinstance(amenity_obj, Amenity):
+                    if amenity_obj.id not in self.amenity_ids:
+                        self.amenity_ids.append(amenity_obj.id)
