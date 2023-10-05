fbd55b6569fdeada91e8c8a7742b265957fd19b5
XimeonLeo <ximeonleo@gmail.com>
2023-10-05 06:55:57 +0100

    extra files

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- A script that prepares MySQL database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- create a user IN localhost
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Granting all permission to user on this database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
