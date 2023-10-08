f670cb3090f449c37982fccefa4df55f430630e8
XimeonLeo <ximeonleo@gmail.com>
2023-10-08 01:35:57 +0100

    removed do pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,29 +8,6 @@ import os
 
 
 @task
-def do_pack():
-    """ A method that creates the archive """
-    time = datetime.now()
-    dt = "{}{}{}{}{}{}".format(time.year,
-                               time.month,
-                               time.day,
-                               time.hour,
-                               time.minute,
-                               time.second)
-    path = "versions/web_static_{}.tgz".format(dt)
-    print("Packing web_static to {}".format(path))
-
-    local("mkdir -p versions")
-
-    if local("tar -czvf {} web_static".format(path)).succeeded:
-        return path
-    return None
-
-
-env.hosts = ['54.162.80.16', '54.236.45.116']
-
-
-@task
 def do_deploy(archive_path):
     """ Destribute an archive to my web servers
     """
