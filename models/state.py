24888b0d79905aaa4ae3f0eb94783a316f2195ba
XimeonLeo <ximeonleo@gmail.com>
2023-10-22 09:44:55 -0700

    DEbugging

--- models/state.py
+++ models/state.py
@@ -4,6 +4,8 @@ import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage_type
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -15,10 +17,13 @@ class State(BaseModel, Base):
 
     if storage_type == "db":
         """ Using DBStorage """
+        name = Column(String(128), nullable=False)
         cities = relationship("City",
                               backref="state",
                               cascade="all, delete")
     else:
+        name = ''
+
         @property
         def cities(self):
             """ Returns the list of City instance where
@@ -26,10 +31,15 @@ class State(BaseModel, Base):
 
                 Using FileStorage
             """
-            from models import storage, City
-
-            cities = []
-            for city in storage.all(City).values():
+            from models import storage
+            like_cities = []
+            """ for city in storage.all(City).values():
                 if city.state_id == self.id:
                     cities.append(city)
-            return cities
+            return cities """
+
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    like_cities.append(city)
+            return like_cities
