0a01717f02d0fd9354d345f70f0ac33b0c726be5
XimeonLeo <ximeonleo@gmail.com>
2023-10-22 10:12:08 -0700

    Foundf the bugg

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,15 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        """ return FileStorage.__objects """
+        if cls is None:
+            return self.__objects
+        cls_name = cls.__name__
+        dct = {}
+        for key in self.__objects.keys():
+            if key.split('.')[0] == cls_name:
+                dct[key] = self.__objects[key]
+                return dct
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
